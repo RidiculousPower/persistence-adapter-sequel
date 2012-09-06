4d0c8cbbbb07b58f5d359e903038c1e70b035cce
Conner <cmtoups@me.com>
2012-09-06 03:00:06 -0400

    Spec now connects with mysql. All test fail do to issue with symbol cannot be converted to string issue.

--- spec/Persistence/Adapter/Sequel/mysql_spec.rb
+++ spec/Persistence/Adapter/Sequel/mysql_spec.rb
@@ -3,9 +3,9 @@ require_relative '../../../../lib/persistence/adapter/sequel.rb'
 
 describe ::Persistence::Adapter::Sequel do
 
-
+	#gem install mysqlplus -- --with-mysql-config=/usr/local/Cellar/mysql/5.1.51/bin/mysql_config
   
-  $__persistence__spec__adapter__ = ::Persistence::Adapter::Sequel.new( :adapter=>:mysql, :database=> :test, :host => :localhost )
+  $__persistence__spec__adapter__ = ::Persistence::Adapter::Sequel.new( :adapter => :mysql, :database => :test )
 
   
   # adapter spec
