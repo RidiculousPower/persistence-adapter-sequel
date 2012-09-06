2b98f1d7c801de3be3790da7cdf76cb6bee1ce55
Conner <cmtoups@me.com>
2012-09-05 22:47:56 -0400

    Returning tests to default setup. Future advanced testing should be implemented at a latter date.

--- spec/Persistence/Adapter/Sequel/sequel_spec.rb
+++ spec/Persistence/Adapter/Sequel/sequel_spec.rb
@@ -5,18 +5,10 @@ describe ::Persistence::Adapter::Sequel do
 
 
   
-  $__persistence__spec__adapter__ = ::Persistence::Adapter::Sequel.new( :adapter=>:postgres, :database=> :test )
+  $__persistence__spec__adapter__ = ::Persistence::Adapter::Sequel.new( :adapter=>:postgres, :database=> :test, :host => :localhost )
 
-#Curently not fuctionall, rollback configure must be done in persistence itself.
-  RSpec.configure do |c|
-      def execute(*args, &block)
-        result = nil
-        Sequel::Model.db.transaction(:rollback=>:always){result = super(*args, &block)}
-        result
-      end
-  end
   
   # adapter spec
-  #require_relative File.join( ::Persistence::Adapter::Abstract.spec_location, 'Adapter_spec.rb' )
+  require_relative File.join( ::Persistence::Adapter::Abstract.spec_location, 'Adapter_spec.rb' )
 
 end
