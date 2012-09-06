e32bef8e252fb09e6a5f654ab1f45e70e121c76b
Conner <cmtoups@me.com>
2012-09-06 06:28:30 -0400

    This adapter is fully functional!

--- spec/Persistence/Adapter/Sequel/swift_spec.rb
+++ spec/Persistence/Adapter/Sequel/swift_spec.rb
@@ -5,7 +5,7 @@ describe ::Persistence::Adapter::Sequel do
 
 
   
-  $__persistence__spec__adapter__ = ::Persistence::Adapter::Sequel.new( :adapter => 'swift')
+  $__persistence__spec__adapter__ = ::Persistence::Adapter::Sequel.new( :adapter => 'swift', :db_type => 'sqlite', :database => '/tmp/testing')
 
   
   # adapter spec
