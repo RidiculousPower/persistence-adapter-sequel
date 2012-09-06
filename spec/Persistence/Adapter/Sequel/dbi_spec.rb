2546b275369f39f1fc7dfbbb0a5904597e0469ef
Conner <cmtoups@me.com>
2012-09-06 04:12:25 -0400

    Added a unique test for and the basic adapter name for each Sequel adapter. Most test should fail as not all the information has been supplied.

---
+++ spec/Persistence/Adapter/Sequel/dbi_spec.rb
@@ -0,0 +1,14 @@
+
+require_relative '../../../../lib/persistence/adapter/sequel.rb'
+
+describe ::Persistence::Adapter::Sequel do
+
+
+  
+  $__persistence__spec__adapter__ = ::Persistence::Adapter::Sequel.new( :adapter => 'dbi')
+
+  
+  # adapter spec
+  require_relative File.join( ::Persistence::Adapter::Abstract.spec_location, 'Adapter_spec.rb' )
+
+end
